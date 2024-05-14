649befc5b30ab4ef873c4ff7bf66c8518e3bd57e
svlipatov <svlipatov@bk.ru>
2024-05-14 11:24:33 +0300

    new

--- load_interface.py
+++ load_interface.py
@@ -1,20 +1,40 @@
 import pandas as pd
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
+    # QWidget
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Load params")
+        self.centralWidget = QWidget()  # +++
+        self.setCentralWidget(self.centralWidget)
+        self.statusBar().showMessage("")
+        # Создадим меню
+        # Кнопка сохранить
+        sAction = QAction('Save', self)
+        sAction.setShortcut('Ctrl+S')
+        sAction.setStatusTip('Save the document')
+        sAction.triggered.connect(self.save_btn_click)
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+        fileMenu.addAction(sAction)
+
         self.containers_qua = 0
         self.btn_plus_dict = {}
         self.btn_num = 0
         self.low_num = 0
         self.entries = {}
 
-        l = QVBoxLayout()
+        l =  QVBoxLayout(self.centralWidget)
         self.l = l
+        p = QPixmap('filter.png')
+        self.pic = QLabel(self)
+        self.pic.adjustSize()
+        self.pic.setPixmap(p)
+        l.addWidget(self.pic)
         # Выпадающий список
         self.cbox = QComboBox()
         self.cbox.addItems(loads)
@@ -49,8 +69,6 @@ class MainWindow(QWidget):
         self.containers_qua += 1
         self.left_list.insertItem(0, 'Параметры выгрузки №' + str(self.containers_qua))
         setattr(self, 'container' + '_' + str(self.containers_qua), QWidget())
-        print('container' + '_' + str(self.containers_qua))
-        print(getattr(self, 'container' + '_' + str(self.containers_qua)))
         self.container_ui()
         self.stack.addWidget(getattr(self, 'container' + '_' + str(self.containers_qua)))
 
@@ -59,13 +77,18 @@ class MainWindow(QWidget):
         layout = QFormLayout()
         for filter in self.df_load_par['FILTER']:
             layout_line = QHBoxLayout()
-            layout_line.addWidget(QLabel(filter))
+            label = QLabel(filter)
+            # Стили
+            label.setStyleSheet(
+            'font-family: Times New Roman;'
+            'font-size: 12px;'
+            'font-weight: bold;')
+            layout_line.addWidget(label)
             layout_line.addWidget(self.add_low(filter))
             layout_line.addWidget(self.add_button(layout, filter))
+            layout_line.addWidget(self.add_button_minus(layout, layout.count()))
             layout.addRow(layout_line)
         att = getattr(self, 'container' + '_' + str(self.containers_qua))
-        print('container' + '_' + str(self.containers_qua))
-        print(att)
         att.setLayout(layout)
     def display(self, i):
         # Отображение только нужного контейнера
@@ -76,10 +99,17 @@ class MainWindow(QWidget):
         btn_layout = self.btn_plus_dict[btn][0]
         btn_filter = self.btn_plus_dict[btn][1]
         layout_line = QHBoxLayout()
-        layout_line.addWidget(QLabel(btn_filter))
+        label = QLabel(btn_filter)
+        label.setStyleSheet(
+            'font-family: Times New Roman;'
+            'font-size: 12px;'
+            'font-weight: bold;')
+        layout_line.addWidget(label)
         layout_line.addWidget(self.add_low(btn_filter))
         layout_line.addWidget(self.add_button(btn_layout, btn_filter))
+        layout_line.addWidget(self.add_button_minus(btn_layout, btn_layout.count()))
         btn_layout.addRow(layout_line)
+
 
     def add_button(self, bt_layout, bt_filter):
         # Создание кнопки добавления еще одной строки с фильтром
@@ -87,7 +117,6 @@ class MainWindow(QWidget):
         setattr(self, 'btn' + '_' + str(self.btn_num), QPushButton("+"))
         cur_btn = getattr(self, 'btn' + '_' + str(self.btn_num), QPushButton("+"))
         cur_btn.clicked.connect(lambda x: self.add_row_btn(cur_btn))
-        print(cur_btn, bt_layout, bt_filter)
         self.btn_plus_dict[cur_btn] = [bt_layout, bt_filter]
         return cur_btn
 
@@ -111,7 +140,19 @@ class MainWindow(QWidget):
             data_list.append(self.entries[lines])
         df_filters = pd.DataFrame(data_list, columns = ['load_num', 'load_field', 'value'])
         df_filters.to_csv('filters/filter.csv', index=False)
+        self.statusBar().showMessage('Фильтры сохранены')
         print(df_filters)
+
+    def add_button_minus(self, bt_layout, num):
+        # Создание кнопки добавления еще одной строки с фильтром
+        setattr(self, 'btn_min' + '_' + str(self.btn_num), QPushButton("-"))
+        cur_btn_min = getattr(self, 'btn_min' + '_' + str(self.btn_num), QPushButton("-"))
+        cur_btn_min.clicked.connect(lambda x: self.delete_row(bt_layout, num))
+        return cur_btn_min
+
+    def delete_row(self, bt_layout, num):
+        bt_layout.removeRow(num)
+
 
 # Соответствие тех имени загрузки и ее возможных фильтров
 df_all_load_par = pd.read_csv("load_parameters/params.csv", delimiter= ';')
